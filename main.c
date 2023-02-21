a9fcf71029814f399135e325d49eaea237a7361b
Tristan <tboda@gaming.tech>
2023-02-21 17:35:46 +0100

    start of display

--- main.c
+++ main.c
@@ -1,24 +1,55 @@
 #include <stdio.h>
 
-//  15 = ☼
+void display(l,h){/*===============ATTENTION (pas de type donc check si erreur)=======================================*/
+    printf("  ");
+    for (int i=0;i<l;i++)
+    {
+        if(i<9)
+        {
+            printf("%c ",186);
+        }else{
+            printf("%c%d ",186,(i+1)/10);
+        }
+    }
+
+    printf("\n  ");
+
+    for (int i=0;i<l;i++)
+    {
+        printf("%c%d",186,(i-((i+1)/10)*10)+1);
+    }
+
+
+    for (int i=0;i<h;i++)
+    {
+        printf("\n%c%c",205,205);
+        for (int j=0;j<l;j++)
+        {
+            printf("%c%c",206,205);
+        }
+        printf("\n %d",i);
+        for (int j=0;j<l;j++)
+        {
+            printf("%c%c",186,219);
+        }
+    }
+}
+
+
+
+
+
+
+
 
 int main()
 {
-    int l, L;
+    int l, h;
 
     printf("Entrer longueur de grille : "); scanf("%d", &l);
-    printf("Entrer hauteur de grille : "); scanf("%d", &L);
+    printf("Entrer hauteur de grille : "); scanf("%d", &h);
 
-    char board[l*L];
-
-    for(int i = 0; i < l*L; i++)
-    {
-        printf("%dth element : %c\n", i, board[i]);
-    }
-
-    printf("###\n");
-    printf("###\n");
-    printf("###\n");
-
+    char board[l*h];
+    display(l,h);
     return 0;
 }

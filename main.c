3e6bdb79042d96ba5e51ba355d90513b66e92cf7
Tristan <tboda@gaming.tech>
2023-02-21 18:54:08 +0100

    display restart

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void display(l,h){/*===============ATTENTION (pas de type donc check si erreur)=
         {
             printf("%c ",186);
         }else{
-            printf("%c%d ",186,(i+1)/10);
+            printf("%c%d",186,(i+1)/10);
         }
     }
 
@@ -27,10 +27,19 @@ void display(l,h){/*===============ATTENTION (pas de type donc check si erreur)=
         {
             printf("%c%c",206,205);
         }
-        printf("\n %d",i);
-        for (int j=0;j<l;j++)
-        {
-            printf("%c%c",186,219);
+
+        if (i<9){
+            printf("\n %d",i+1);
+            for (int j=0;j<l;j++)
+            {
+                printf("%c%c",186,219);
+            }
+        }else{
+            printf("\n%d",i+1);
+            for (int j=0;j<l;j++)
+            {
+                printf("%c%c",186,219);
+            }
         }
     }
 }
@@ -38,16 +47,25 @@ void display(l,h){/*===============ATTENTION (pas de type donc check si erreur)=
 
 
 
-
-
+/*
+bomb = 0 / 1
+flag/hidden/show = 1 / 2 / 3
+state bomb
+10 / 20 / 30 / 11 / 21 / 31
+*/
 
 
 int main()
 {
-    int l, h;
-
-    printf("Entrer longueur de grille : "); scanf("%d", &l);
+    int l=3, h=3;
+    int * tableau[l*h] ={31,30,20,21,20,20,11,10,20}
+    /*printf("Entrer longueur de grille : "); scanf("%d", &l);
     printf("Entrer hauteur de grille : "); scanf("%d", &h);
+    while (l>99||h>99)
+    {
+        printf("Entrer longueur de grille : "); scanf("%d", &l);
+        printf("Entrer hauteur de grille : "); scanf("%d", &h);
+    }*/
 
     char board[l*h];
     display(l,h);

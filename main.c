ec07e12cc98e4566b60beeb9924ec13afb9854d5
Tristan <tboda@gaming.tech>
2023-02-23 09:03:44 +0100

    Display

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void display(int l,int h,int*tableau){/*===============ATTENTION (pas de type donc check si erreur)=======================================*/
+void display(int l,int h,int*tableau){
     int bomb=15, flag=127, hidden=219,show=177;
     printf("  ");
     for (int i=0;i<l;i++)
@@ -52,7 +52,17 @@ void display(int l,int h,int*tableau){/*===============ATTENTION (pas de type do
 }
 
 
+int main()
+{
+    srand( time( NULL ));
+    int l=5, h=5;
+    int tableau[9]={};
 
+
+    display(l,h,tableau);
+    char board[l*h];
+    return 0;
+}
 
 /*
 bomb = 0 / 1
@@ -60,23 +70,3 @@ flag/show/hidden = 1 / 2 / 3
 state bomb
 10 / 20 / 30 / 11 / 21 / 31
 */
-
-
-int main()
-{
-    int l=3, h=3;
-    int tableau[9];
-    for (int i=0;i<=9;i++){
-
-    }
-    /*printf("Entrer longueur de grille : "); scanf("%d", &l);
-    printf("Entrer hauteur de grille : "); scanf("%d", &h);
-    while (l>99||h>99)
-    {
-        printf("Entrer longueur de grille : "); scanf("%d", &l);
-        printf("Entrer hauteur de grille : "); scanf("%d", &h);
-    }*/
-    display(l,h,tableau);
-    char board[l*h];
-    return 0;
-}
